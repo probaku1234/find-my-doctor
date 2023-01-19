0196889a3d73c51a070fb27c2ae61bd9fb5bfe3c
Yun Seob Lee <fhzotxldj@gmail.com>
2023-01-19 15:44:13 +0900

    implement model schema

--- findMyDoctor/models.py
+++ findMyDoctor/models.py
@@ -1,8 +1,9 @@
-from djongo import models
-from django import forms
+from django.db import models
+from datetime import datetime
 
 
 class Patient(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=70, blank=False, default='')
 
     def __str__(self):
@@ -14,49 +15,53 @@ class BusinessHour(models.Model):
     end = models.TimeField()
     closed = models.BooleanField()
 
-    class Meta:
-        abstract = True
-
-
-class BussinessHourForm(forms.ModelForm):
-    class Meta:
-        model = BusinessHour
-        fields = (
-            'start', 'end', 'closed'
-        )
+    def __str__(self):
+        if self.closed:
+            return '휴무'
+        else:
+            return self.start.__str__() + ' ~ ' + self.end.__str__()
 
 
 class MedicalDepartment(models.Model):
     name = models.CharField(max_length=70, blank=False, default='')
 
-    class Meta:
-        abstract = True
-
-
-class MedicalDepartmentForm(forms.ModelForm):
-    class Meta:
-        model = MedicalDepartment
-        fields = (
-            'name',
-        )
-
-
-class Doctor(models.Model):
-    name = models.CharField(max_length=70, blank=False, default='')
-    hospitalName = models.CharField(max_length=70, blank=False, default='')
-    medicalDepartment = models.ArrayField(model_container=MedicalDepartment, model_form_class=MedicalDepartmentForm)
-    noImbursedMedicalDepartment = models.ArrayField(model_container=MedicalDepartment, model_form_class=MedicalDepartmentForm)
-    businessHour = models.ArrayField(model_container=BusinessHour, model_form_class=BussinessHourForm)
-
     def __str__(self):
         return self.name
 
 
-class TreatmentRequest(models.Model):
-    patientId: models.ForeignKey(Patient, on_delete=models.CASCADE)
-    doctorId: models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    time: models.DateTimeField()
-    createdAt: models.DateTimeField()
-    expiredAt: models.DateTimeField()
-    isAccepted: models.BooleanField()
+class Doctor(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=70, blank=False, default='')
+    hospitalName = models.CharField(max_length=70, blank=False, default='')
+    medicalDepartment = models.ManyToManyField(MedicalDepartment, related_name='medicalDepartment', blank=True,
+                                               null=True)
+    noImbursedMedicalDepartment = models.ManyToManyField(MedicalDepartment, related_name='noImbursedMedicalDepartment',
+                                                         blank=True, null=True)
+    businessHourMon = models.ForeignKey(BusinessHour, related_name='businessHourMon', on_delete=models.CASCADE,
+                                        blank=False)
+    businessHourTue = models.ForeignKey(BusinessHour, related_name='businessHourTue', on_delete=models.CASCADE,
+                                        blank=False)
+    businessHourWed = models.ForeignKey(BusinessHour, related_name='businessHourWed', on_delete=models.CASCADE,
+                                        blank=False)
+    businessHourThu = models.ForeignKey(BusinessHour, related_name='businessHourThu', on_delete=models.CASCADE,
+                                        blank=False)
+    businessHourFri = models.ForeignKey(BusinessHour, related_name='businessHourFri', on_delete=models.CASCADE,
+                                        blank=False)
+    businessHourSat = models.ForeignKey(BusinessHour, related_name='businessHourSat', on_delete=models.CASCADE,
+                                        blank=False)
+    businessHourSun = models.ForeignKey(BusinessHour, related_name='businessHourSun', on_delete=models.CASCADE,
+                                        blank=False)
+    lunchTime = models.ForeignKey(BusinessHour, related_name='lunchTime', on_delete=models.CASCADE, blank=False)
 
+    def __str__(self):
+        return self.name + ', ' + self.hospitalName
+
+
+class TreatmentRequest(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    patientId = models.ForeignKey(Patient, blank=False, on_delete=models.CASCADE, related_name='patientId')
+    doctorId = models.ForeignKey(Doctor, blank=False, on_delete=models.CASCADE, related_name='doctorId')
+    time = models.DateTimeField(blank=False)
+    createdAt = models.DateTimeField(blank=True, default=datetime.now)
+    expiredAt = models.DateTimeField()
+    isAccepted = models.BooleanField(blank=False, default=False)
